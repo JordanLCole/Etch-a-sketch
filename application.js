90eb37961d71e8fd6fc0b74ad53027a4ce2f9965
Jordan <jordancole2245@gmail.com>
2022-09-11 11:22:14 +0100

    Fixed create-grid functionality

--- application.js
+++ application.js
@@ -1,20 +1,48 @@
 "use strict";
 
-//document elements
+//Page-layout
 const gridContainer = document.querySelector(".grid-container");
+const controlPanel = document.querySelector(".control-panel");
+const createGridButton = document.querySelector("#create-grid");
 
-//buttons
-const createGrid = document.getElementById("create-grid");
+let squaresSide = 16;
+let cell = [];
 
 //event listeners
-createGrid.addEventListener("click", function () {
-  console.log("button clicked");
-  let gridSize = document.querySelector("input").valueAsNumber;
-  gridContainer.style.setProperty("--grid-rows", gridSize);
-  console.log(gridSize);
-  gridContainer.style.setProperty("--grid-cols", gridSize);
-  for (let i = 0; i < gridSize * gridSize; i++) {
-    let cell = document.createElement("div");
-    gridContainer.appendChild(cell).className = "grid-item";
+createGridButton.addEventListener("click", function () {
+  //clear();
+  createGrid(newSize.value);
+});
+
+//set grid size
+let newSize = document.querySelector(".grid-size-input");
+newSize.value = 16;
+
+//initial grid display
+
+createGrid(squaresSide);
+
+//function
+
+function createGrid(squaresSide) {
+  removeCells();
+  gridContainer.style.gridTemplateColumns = `repeat(${squaresSide}, 1fr`;
+  gridContainer.style.gridTemplateRows = `repeat(${squaresSide}, 1fr`;
+  let numberOfCells = squaresSide * squaresSide;
+  for (let i = 0; i < numberOfCells; i++) {
+    cell[i] = document.createElement("div");
+    cell[i].classList.add("cell");
+    cell[i].style = "background-color: rgba(255, 255, 255, 1)";
+    gridContainer.appendChild(cell[i]);
   }
+}
+
+function removeCells() {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+}
+
+cell.addEventListener("mousedown", function () {
+  cell.classList.add("filled-cell");
 });
